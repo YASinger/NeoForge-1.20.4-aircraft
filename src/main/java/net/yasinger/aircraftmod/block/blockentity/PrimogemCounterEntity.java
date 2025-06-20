d846ebb4bf8cff5962b2c85799191f49e6f00b24
YASinger <2957162290@qq.com>
2025-06-20 17:59:33 +0800

    [0.00.08] 新增方块实体保存nbt,新增方块实体tick方法

--- src/main/java/net/yasinger/aircraftmod/block/blockentity/PrimogemCounterEntity.java
+++ src/main/java/net/yasinger/aircraftmod/block/blockentity/PrimogemCounterEntity.java
@@ -1,6 +1,10 @@
 package net.yasinger.aircraftmod.block.blockentity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
@@ -8,6 +12,8 @@ import net.yasinger.aircraftmod.block.ModBlockEntities;
 
 public class PrimogemCounterEntity extends BlockEntity {
     private int count = 0;
+    private static final int MAX_COUNT = 20 * 2;
+    private int tickCount = 0;
     public PrimogemCounterEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.PRIMOGEM_COUNTER_ENTITY.get(), pPos, pBlockState);
     }
@@ -15,5 +21,28 @@ public class PrimogemCounterEntity extends BlockEntity {
         count++;
         setChanged();
         return count;
+    }
+    public static void serverTick(Level level, BlockPos pos, BlockState state, PrimogemCounterEntity entity) {
+        if(level != null && !level.isClientSide()) {
+            if(entity.tickCount == MAX_COUNT) {
+                entity.tickCount = 1;
+                Player nearestPlayer = level.getNearestPlayer(pos.getX(), pos.getY(), pos.getZ(), 3, false);
+                if (nearestPlayer != null) {
+                    nearestPlayer.sendSystemMessage(Component.literal("Primogem Counter: " + entity.count));
+                }
+            } else {
+                entity.tickCount++;
+            }
+        }
+    }
+    @Override
+    public void load(CompoundTag tag) {
+        count = tag.getInt("count");
+        super.load(tag);
+    }
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.putInt("count", count);
     }
 }
--- src/main/java/net/yasinger/aircraftmod/block/custom/PrimogemCounterBlock.java
+++ src/main/java/net/yasinger/aircraftmod/block/custom/PrimogemCounterBlock.java
@@ -1,21 +1,27 @@
 package net.yasinger.aircraftmod.block.custom;
 
 import com.mojang.serialization.MapCodec;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.BeehiveBlock;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.yasinger.aircraftmod.block.ModBlockEntities;
 import net.yasinger.aircraftmod.block.blockentity.PrimogemCounterEntity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class PrimogemCounterBlock extends BaseEntityBlock {
     public PrimogemCounterBlock() {
@@ -40,6 +46,24 @@ public class PrimogemCounterBlock extends BaseEntityBlock {
     }
     @Override
     protected @NotNull MapCodec<? extends BaseEntityBlock> codec() {
+        return null;
+    }
+@Nullable
+@Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(
+            @NotNull Level level,
+            @NotNull BlockState state,
+            @NotNull BlockEntityType<T> type) {
+        if (level.isClientSide()) {
+            return null;
+        }
+        if (type == ModBlockEntities.PRIMOGEM_COUNTER_ENTITY.get()) {
+            return (lvl, pos, st, be) -> {
+                if (be instanceof PrimogemCounterEntity primogemCounterEntity) {
+                    PrimogemCounterEntity.serverTick(lvl, pos, st, primogemCounterEntity);
+                }
+            };
+        }
         return null;
     }
 }
